054668a9fd7b046715f4b9cbcd0e15968ce08228
Aquiles <aquilesfalcaoo@gmail.com>
2021-12-04 17:15:57 -0300

    adicionado os métodos findAll e findOne

--- app.js
+++ app.js
@@ -5,20 +5,53 @@ const connection = new Sequelize("db_demo", "root", "root", {
   dialect: "mysql",
 });
 
+//* Realizando a Conexão
+const conexao = connection
+  .authenticate()
+  .then(function () {
+    console.log("Conexão realizada com sucesso!");
+  })
+  .catch(function (err) {
+    console.log("Não foi possível se conectar!");
+  })
+  .done();
+
+//* Criando a Tabela no Banco de Dados
 const Artigos = connection.define("artigos", {
   titulo: Sequelize.STRING,
   assunto: Sequelize.TEXT,
 });
 
+//? Evita de Criar novas Tabelas no Banco de Dados ao Executar
+// connection.sync().then(function () {
+//   Artigos.create({
+//     titulo: "Manipulando Sequelize",
+//     assunto: "Manipulação de Objetos no Sequelize",
+//   });
+// });
+
+//! Método findAll
 connection.sync().then(function () {
-  Artigos.create({
-    titulo: "Testando o Sequelize",
-    assunto: "Neste artigo iremos testar os recursos do Sequelize",
+  const artigos = Artigos.findAll().then(function (artigos) {
+    console.log(
+      "===================================================================================================================="
+    );
+    console.log("Quantidade: " + artigos.length);
+    artigos.forEach((element) => {
+      console.log(element.titulo + " - " + element.assunto);
+    });
+    console.log(artigos[0]);
   });
 });
 
+//! Método findOne
+console.log(
+  "===================================================================================================================="
+);
 connection.sync().then(function () {
-  Artigos.findAll().then(function (artigos) {
-    console.log(artigos.dataValues);
-  });
+  Artigos.findOne({ where: { titulo: "manipulando Sequelize" } }).then(
+    function (artigo) {
+      console.log("Título: " + artigo.titulo + " - Assunto: " + artigo.assunto);
+    }
+  );
 });
