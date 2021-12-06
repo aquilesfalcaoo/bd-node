06e46bd02ba44ef53b2898cfd92f718870783101
Adila Souza <adila753@gmail.com>
2021-12-05 22:57:00 -0300

    espacamento

--- examples/deleteOne.js
+++ examples/deleteOne.js
@@ -1,6 +1,6 @@
 const { Diagramas } = require('../models');
 
 (async () => {
-    const resultado = await Diagramas.destroy({ where: { id:1 } });
+    const resultado = await Diagramas.destroy({ where: { id: 1 } });
     console.log(resultado);
 })();
